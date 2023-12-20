0afa6718c335e1a9edf8fbfa3c20df16e58a12fc
root <root@ndeyeUbuntu.myguest.virtualbox.org>
2023-12-20 04:22:24 +0100

    exercice2 last version

--- exercice2.c
+++ exercice2.c
@@ -1,96 +1,97 @@
+#include <ctype.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 #define BUFSIZE 1024
 
-
-
-int parse_line(char *s, char **argv[]){
-    char word[1024];
+int parse_line(char *s, char **argv[]) {
+    char word[BUFSIZE];
     int nbWord = 0;
     int wordIndex = 0;
-    for(int i=0; i < sizeof(s) ; i++){
-        if(s[i] == ' '){
-            *argv[nbWord] = word;
-            nbWord++;
-            wordIndex = 0;  
-        }
-        else{
+
+    // Allouer de l'espace pour le tableau d'arguments
+    *argv = (char **)malloc((BUFSIZE / 2) * sizeof(char *));
+    if (*argv == NULL) {
+        perror("Allocation echouee");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (isspace((unsigned char)s[i]) || s[i] == '\n') {
+            word[wordIndex] = '\0';
+            if (wordIndex > 0) {
+                // Ignorer les espaces supplémentaires entre les mots
+                (*argv)[nbWord] = malloc(strlen(word) + 1);
+                if ((*argv)[nbWord] == NULL) {
+                    perror("Allocation echouee");
+                    exit(EXIT_FAILURE);
+                }
+                strcpy((*argv)[nbWord], word);
+                nbWord++;
+                wordIndex = 0;
+            }
+        } else {
             word[wordIndex] = s[i];
             wordIndex++;
         }
     }
-    write(STDOUT_FILENO, word, sizeof(word));
-    *argv[nbWord] = word;
-    *argv[nbWord + 1] = NULL;
-    write(STDOUT_FILENO, "parsing réussi", 16);
-    return 1;
+
+    (*argv)[nbWord] = NULL;
+
+    return nbWord;
 }
 
-int main(int argc , char * argv[]){
-
+int main(int argc, char *argv[]) {
     int n;
     char prompt[] = {'$'};
     char buffer[BUFSIZE];
-    //int commandSize = 0;
-    //int commandLength = 0;
-    //int finalLength;
-    //int n;
-    //char **args[1024];
-    
+    char **args = NULL;
 
-    while(1){
-        write(STDOUT_FILENO,(void*) prompt, 1);
-        n = read(STDIN_FILENO, (void*) buffer, BUFSIZE);
-        
-	char command[n];
-	for(int i = 0; i < n; i++){
-		command[i] = buffer[i];
-	}
+    while (1) {
+        write(STDOUT_FILENO, (void *)prompt, 1);
+        n = read(STDIN_FILENO, (void *)buffer, BUFSIZE);
 
-       /* for(int i= 0 ;i<1024;i++){
-            if(buffer[i] - '\0' == 0){
-                finalLength = commandLength;
+        if (n != -1) {
+            int argc = parse_line(buffer, &args);
+             
+               
+            if (argc > 0) {
+                 printf("affichons les commandes dans le tableu\n");
+                for (int i = 0; i < argc+1; i++) {
+                    printf(" | %s", args[i]);
+                }
+                printf("\n");
+               
+                if (strcmp(args[0], "exit") == 0) {
+                    printf("Exiting the shell\n");
+                    break;
+                }
+
+                if (fork() == 0) {
+                    execvp(args[0], args);
+                    perror("Exec failed");
+                    exit(EXIT_FAILURE);
+                    for (int i = 0; i < argc+1; i++) {
+                    free(args[i]);
+                }
+                } else {
+                    wait(NULL);
+                }
+
+                // Libérer la mémoire allouée pour les arguments
+                for (int i = 0; i < argc+1; i++) {
+                    free(args[i]);
+                }
+                free(args);
+                args = NULL;
             }
-            else{
-                commandLength++;
-            }
         }
-
-        char bufferOut[finalLength];*/
-
-        //for (int i = 0; i < finalLength; i++){
-            /*bufferOut[i] = buffer[i];
-        }*/
-        
-       
-        //write(STDOUT_FILENO, (void*) "écriture réussie\n", 20);
-	write(STDOUT_FILENO, (void*) command, sizeof(command));
-        
-	//processus fils, qui execute la commande
-        if(fork() == 0){
-            execl("/bin/sh","/bin/sh",  "-c", command, (char*) NULL);
-        }
-	else{
-		wait(NULL);
-	}
-        /*if (n != -1){
-            parse_line(buffer, args);
-        
-        }
-
-        if(fork() == 0){
-            execvp(*args[0], *args);
-        }*/
-
     }
 
-    return 1;
-
+    return 0;
 }
